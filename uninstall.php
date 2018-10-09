0f58cf4c1f786b7f1eb28a8f84589f26e25b0bc2
Kishor Thanth <kishor.thanth@texhseria.com>
2018-10-09 13:51:34 +0530

    move out the code from the inner directory to parent as requested by Matt

--- uninstall.php
+++ uninstall.php
@@ -1,9 +1,5 @@
 <?php
-
 /**
- * NOTICE: This file is kept as a safety-measure on my part (the developer of this plugin). 
- * I may need to use it in a later update and reading the link below makes me think I might as well leave it here for now!
- * 
  * Fired when the plugin is uninstalled.
  *
  * When populating this file, consider the following flow
@@ -16,18 +12,11 @@
  * - Repeat with other user roles. Best directly by using the links/query string parameters.
  * - Repeat things for multisite. Once for a single site in the network, once sitewide.
  *
- * This file may be updated more in future version of the Boilerplate; however, this is the
- * general skeleton and outline for how the file should work.
- *
- * For more information, see the following discussion:
- * https://github.com/tommcfarlin/WordPress-Plugin-Boilerplate/pull/123#issuecomment-28541913
- *
- * @link       http://example.com
- * @since      1.0.0
- *
- * @package    Plugin_Name
+ * @link              https://www.dotstudiopro.com
+ * @since             1.0.0
+ * @package           Dotstudiopro_Api
  */
-
+ 
 // If uninstall not called from WordPress, then exit.
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
