6693eb7117b00994e8ab78b95e03869dc1f33afa
chirag <chirag.rupapara@techseria.com>
2018-10-12 21:02:21 +0530

    Print the IP Address

--- includes/class-dotstudiopro-external-api-requests.php
+++ includes/class-dotstudiopro-external-api-requests.php
@@ -178,6 +178,7 @@ class Dsp_External_Api_Request {
      * @return String The IP address of the user
      */
     private function get_ip() {
+        echo '<pre>'; print_r($_SERVER); exit;
         if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
             //check ip from share internet
             $ip = $_SERVER['HTTP_CLIENT_IP'];
